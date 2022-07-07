5c82cedd68462e6a92e5f1fc48323d0b48edea73
Sarath P <sarath.pariyarath@gmail.com>
2022-07-08 00:08:26 +0530

    Onboarding, Home screen basic ui

---
+++ Restart-App/Screens/OnboardingView.swift
@@ -0,0 +1,123 @@
+//
+//  OnboardingView.swift
+//  Restart-App
+//
+//  Created by Sarath P on 07/07/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    
+    var body: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                //MARK: - HEADER
+                
+                Spacer()
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                         It's not how much we give but how much love we put into giving.
+                         """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                }//MARK: -HEADER
+                
+                //MARK: - CENTER
+                ZStack {
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                    
+                } //MARK: CENTER
+                Spacer()
+                
+                //MARK: - FOOTER
+                ZStack {
+                    //MARK: 1. BACKGROUND (STATIC)
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.3))
+                        .padding(8)
+                    //MARK: 2. CALL-TO-ACITON (STATIC)
+                    Text("Get started")
+                        .font(.system(.title3))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    //MARK: 3. CAPSULE (DYNAMIC)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: buttonOffset + 80)
+                        Spacer()
+                        
+                    }
+                    //MARK: 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .offset(x: buttonOffset)
+                        .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                    buttonOffset = gesture.translation.width
+                                }
+                            })
+                            .onEnded({ gesture in
+                                if buttonOffset > buttonWidth / 2 {
+                                    buttonOffset =  buttonWidth - 80
+                                    isOnboardingActive = false
+                                } else {
+                                    buttonOffset = 0
+                                }
+                                
+                                
+                            })
+                        )//MARK: END OF GESTURE
+                        
+                        Spacer()
+                    }
+                } //MARK: FOOTER
+                .frame(width: buttonWidth ,height: 80, alignment: .center)
+                .padding()
+                
+            } //MARK: - VSTACK
+        } //MARK: - ZSTACK
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
---
+++ Restart-App/Views/CircleGroupView.swift
@@ -0,0 +1,37 @@
+//
+//  CircleGroupView.swift
+//  Restart-App
+//
+//  Created by Sarath P on 07/07/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    //MARK: PROPERTY
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //MARK: -ZSTACK
+
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
